b54fc6a14ae57a051d9bd4cad08b0822efb20339
Punit Mudgal <ssmudgal01@gmail.com>
2023-07-12 08:06:55 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  // BrowserRouter as Router,
+  Routes,
+  Route,
+  HashRouter,
+} from "react-router-dom";
 import Alert from "./components/Alert";
 import { AuthProvider } from "./context/AuthContext";
 import { FilterProductProvider } from "./context/FilterContext";
@@ -22,30 +27,31 @@ function App() {
         <ProductProvider>
           <FilterProductProvider>
             <CartProvider>
-            <Router basename="/e_commerce-main">
-              <Header />
-              <Alert />
-              <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                  <Route
-                   exact path="/e_commerce-main"
-                    element={
-                      <>
-                        <Home /> <Main />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="/singleProduct/:id"
-                    element={<SingleProduct />}
-                  />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="/login" element={<Login />} />
-                  <Route path="/signup" element={<Signup />} />
-                  <Route path="/cart" element={<Cart />} />
-                </Routes>
-              </Suspense>
-            </Router>
+              <HashRouter basename="/e_commerce-main">
+                <Header />
+                <Alert />
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Routes>
+                    <Route
+                      exact
+                      path="/e_commerce-main"
+                      element={
+                        <>
+                          <Home /> <Main />
+                        </>
+                      }
+                    />
+                    <Route
+                      path="/singleProduct/:id"
+                      element={<SingleProduct />}
+                    />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/cart" element={<Cart />} />
+                  </Routes>
+                </Suspense>
+              </HashRouter>
             </CartProvider>
           </FilterProductProvider>
         </ProductProvider>
